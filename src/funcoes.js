6289098b5d38f8f4b5ba9f7fce21c66e3ea2fca3
joaovictooorr <joaovitorwf01020304@gmail.com>
2024-09-06 16:54:00 -0300

    typescript

---
+++ src/funcoes.js
@@ -0,0 +1,9 @@
+"use strict";
+function mutiplica(a, b) {
+    return a * b;
+}
+function concatena(nome) {
+    return "Olá " + nome;
+}
+console.log(mutiplica(5, 5));
+console.log(concatena("joão"));
---
+++ src/funcoes.ts
@@ -0,0 +1,10 @@
+function mutiplica(a: number, b: number): number{
+    return a * b;
+}
+
+function concatena(nome: string) : string{
+    return "Olá " + nome;
+}
+
+console.log(mutiplica(5,5));
+console.log(concatena("joão"));
